955423e9b31dd5631a04db5a191d3e4fd1a499f4
Youngsuk <djfls0304@naver.com>
2022-08-19 22:48:32 +0900

    API
    api 받아 DB user_bio_info형식에 맞춰서 저장하기

--- Backend/app/src/api/GetAPI.js
+++ Backend/app/src/api/GetAPI.js
@@ -1,86 +1,108 @@
+import fetch from 'node-fetch';
 
-// const url = "http://210.104.190.229:8381/";
-// fetch("http://210.104.190.229:8381/member/login", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify({
-//     "compSeq": 1, 
-//     "id": "pang7163y@gmail.com",
-//     "lang": "ko_KR",
-//     "pw": "Zxcasd123!",
-//     "sns" : "O"
-//   }),
-// }).then((response) => response.json())
-// .then((data) => {
-//   console.log('성공:', data);
-// })
-// .catch((error) => {
-//   console.error('실패:', error);
-// });
-
-// fetch('http://210.104.190.229:8381/member/login')
-//   .then(res => {
-//     // response 처리
-//     console.log(res);
-//     // 응답을 JSON 형태로 파싱
-//     return res.json();
-//   })
-//   .then(data => {
-//     // json 출력
-//     console.log(data)
-//   })
-//   .catch(err => {
-//     // error 처리
-//     console.log('Fetch Error', err);
-//   });
-
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then(res => {
-//     // response 처리
-//     console.log(res);
-//     // 응답을 JSON 형태로 파싱
-//     return res.json();
-//   })
-//   .then(data => {
-//     // json 출력
-//     console.log(data)
-//   })
-//   .catch(err => {
-//     // error 처리
-//     console.log('Fetch Error', err);
-//   });
-const express = require('express');
-const router = express.Router();
-const request = require('request');
-
-
-let OSDOptions = {
-  url: "http://210.104.190.229:8381/member/login",  
-  method: 'POST',
+var bi_info={
+  
+};
+var member_seq ;
+var member_phone_num;
+var member_email;
+var token;
+var info_time;
+const get_token_url="http://210.104.190.229:8381/login";
+fetch(get_token_url ,{
+  method : "POST",
   headers: {
-    "Content-Type": "application/json",
+      "Content-Type": "application/json",
   },
-  body: JSON.stringify({
-    compSeq : 1, 
-    id : "pang7163y@gmail.com",
-    lang : "ko_KR",
-    pw : "Zxcasd123!",
-    sns : "O"
-  }),
-}
+  body:JSON.stringify({
+    "compSeq": 47, 
+    "id": "dothebestmoon1016@gmail.com",
+    "lang": "ko_KR",
+    "pw": "Hustar1016!",
+    "sns" : "O"
+  })
+}).then((response) => response.json())
+.then((data) => {
+    member_seq=JSON.stringify(data.profile.memberSeq);
+    member_phone_num=JSON.stringify(data.profile.phone);
+    member_email=JSON.stringify(data.profile.email);
+    token=JSON.stringify(data.token);
+    console.log("member_seq : ",member_seq);
+    console.log("member_seq type: ",typeof(member_seq) );
 
-request(OSDOptions, function (err, res, body) {
-  if (!err && res.statusCode == 200) {
-    console.log(JSON.parse(body));
-  }else if (!err && res.statusCode == 401) {
-    console.log("비밀번호 불일치");
-  }else if (!err && res.statusCode == 404) {
-    console.log("해당 계정 없음");
-  }else{
-    console.log("false");
-  }
+    console.log("member_phone_num : ",member_phone_num);
+    console.log("member_email : ",member_email);
+    console.log("token : ",token);
 })
+.catch((error) => {
+    console.error('실패:', error);
+});
 
-module.exports = router;
+const Bp_url = "http://210.104.190.229:8381/v19blood/list?";
+
+fetch(Bp_url + new URLSearchParams({
+  "memberSeq":parseFloat("2345")  ,
+  "startDay": 1660618500000,
+    "endDay": 1660618800000
+}),
+{
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMzM5IiwiZXhwIjoxNjYzMzMxMDg0fQ.QOf-gLZuySGR8MygYUZ6aiEr9xpCTvGZweVaobvw5ObpeB4TYfWD5FbCT4esWKrcPnjNbC6J1ztHkQ8JKa00qw"
+    },
+}).then((response) => response.json())
+.then((data) => {
+  console.log('bp성공',data);
+  bi_info.member_diastolic_blood_pressure=JSON.stringify(data.min[0].diastolic);
+  bi_info.member_systolic_blood_pressure=JSON.stringify(data.max[0].systolic)
+  console.log("diastolic : ",bi_info.member_systolic_blood_pressure);
+  console.log("systolic : ",bi_info.member_diastolic_blood_pressure);
+})
+.catch((error) => {
+  console.error('실패:', error);
+});
+
+const Bpm_url = "http://210.104.190.229:8381/v19heart/list?";
+
+fetch(Bpm_url + new URLSearchParams({
+  "memberSeq": 2345,
+  "startDay": 1660610500000,
+  "endDay": 1660618800000
+}),
+{
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMzM5IiwiZXhwIjoxNjYzMzMxMDg0fQ.QOf-gLZuySGR8MygYUZ6aiEr9xpCTvGZweVaobvw5ObpeB4TYfWD5FbCT4esWKrcPnjNbC6J1ztHkQ8JKa00qw"
+    },
+}).then((response) => response.json())
+.then((data) => {
+  console.log('bpm 성공');
+  bi_info.member_bpm=JSON.stringify(data.list[0].bpm);
+  console.log("bpm : ",bi_info.member_bpm);
+})
+.catch((error) => {
+  console.error('실패:', error);
+});
+
+const Stress_url = "http://210.104.190.229:8381/v19Stress/list?";
+
+fetch(Stress_url + new URLSearchParams({
+  "memberSeq": member_seq,
+  "dte": 1660575600000
+}),
+{
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMzM5IiwiZXhwIjoxNjYzMzMxMDg0fQ.QOf-gLZuySGR8MygYUZ6aiEr9xpCTvGZweVaobvw5ObpeB4TYfWD5FbCT4esWKrcPnjNbC6J1ztHkQ8JKa00qw"
+    },
+}).then((response) => response.json())
+.then((data) => {
+  console.log('stress 성공:');
+  bi_info.member_stress_level=JSON.stringify(data.list[0].stressLevel);
+  bi_info.member_stress_value=JSON.stringify(data.list[0].stressValue);
+  console.log("stress_level : ",bi_info.member_stress_level);
+  console.log("stress_value : ",bi_info.member_stress_value);
+})
+.catch((error) => {
+  console.error('실패:', error);
+});
